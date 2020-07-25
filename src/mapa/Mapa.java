3e42dc43a2f41ad968edb1ef4fa96ebbfdf7be1a
Matheus Neves <mt_nevess@hotmail.com>
2020-07-24 21:39:31 -0300

    mapa mostrando quadrados invisiveis

--- src/mapa/Mapa.java
+++ src/mapa/Mapa.java
@@ -6,6 +6,7 @@ import exception.IllegalMoveException;
 public class Mapa {
 	Prop[][] map;
 	String[][] wallmap;
+	Boolean[][] heromap;
 	int lineLength;
 	int collumnLength;
 	int heroiX = 0, heroiY = 0;
@@ -14,7 +15,8 @@ public class Mapa {
 		this.lineLength = lineLength;
 		this.collumnLength = collumnLength;
 		this.map = new Prop[lineLength][collumnLength];
-		this.wallmap = new String[lineLength][collumnLength];	
+		this.wallmap = new String[lineLength][collumnLength];
+		this.heromap = new Boolean[lineLength][collumnLength];
 		for (int i = 0 ; i < lineLength ; i++) {
 			for (int j = 0 ; j < collumnLength ; j++) {
 				this.map[i][j] = null;
@@ -58,7 +60,50 @@ public class Mapa {
 					this.map[i][j].print();
 				}
 				catch(NullPointerException e) {
-					System.out.printf("░░");
+					if(heromap[i][j]) {
+						System.out.printf("░░");
+					}
+					else {
+						System.out.printf("▓▓");
+					}
+				}
+				
+				if(wallmap[i][j].equals("lado") || wallmap[i][j].equals("baixolado")) {
+					System.out.printf("║");
+				}
+				else {
+					System.out.printf(" ");
+				}
+			}
+			System.out.println();
+			for(int j = 0; j < map[0].length ; j++) {
+				if(wallmap[i][j].equals("baixo") || wallmap[i][j].equals("baixolado")) {
+					System.out.printf("==");
+				}
+				else {
+					System.out.printf("  ");
+				}
+				System.out.printf(" ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public void printHeroMap () {
+		System.out.println();
+		System.out.println();
+		for (int i = 0 ; i < map.length ; i++) {
+			for (int j = 0 ; j < map[0].length ; j++) {
+				try {
+					this.map[i][j].print();
+				}
+				catch(NullPointerException e) {
+					if(heromap[i][j]) {
+						System.out.printf("░░");
+					}
+					else {
+						System.out.printf("▓▓");
+					}
 				}
 				
 				if(wallmap[i][j].equals("lado") || wallmap[i][j].equals("baixolado")) {
@@ -118,10 +163,12 @@ public class Mapa {
 			heroiX -= pos[1];
 			heroiY -= pos[0];
 			map[heroiY][heroiX] = heroi;
-			
+			refreshHeroMap();
+			printHeroMapAAA();			
 			return 1;
  		}
-		
+		refreshHeroMap();
+		printHeroMapAAA();	
 		return 0;
 	}
 	
@@ -154,6 +201,106 @@ public class Mapa {
 			return;
 		}
 			
+	}
+	
+	public void refreshHeroMap() {
+		for (int i = 0 ; i < lineLength ; i++) {
+			for (int j = 0 ; j < collumnLength ; j++) {
+				this.heromap[i][j] = false;
+			}
+		}
+		
+		int i,j;
+		boolean canseefurther;
+		
+		
+		// Visão horizontal para a direita
+		j = heroiX;
+		canseefurther = true;
+		while( j < collumnLength && canseefurther) {
+			heromap[heroiY][j] = true;
+			//System.out.println(map[heroiY][j].symbol);
+			if(map[heroiY][j] != null && j != heroiX) {
+				canseefurther = false;
+			}
+			if(wallmap[heroiY][j] == "lado" || wallmap[heroiY][j] == "baixolado") {
+				canseefurther = false;
+			}
+			else {
+				j++;
+			}
+			
+		}
+		
+		//Visão horizontal para a esquerda
+		j = heroiX;
+		canseefurther = true;
+		while( j >= 0 && canseefurther) {
+			if(map[heroiY][j] != null && j != heroiX) {
+				canseefurther = false;
+					}
+			
+			if(wallmap[heroiY][j] == "lado" || wallmap[heroiY][j] == "baixolado") {
+				canseefurther = false;
+			}
+			else {
+				heromap[heroiY][j] = true;
+				j--;
+			}
+			
+		}
+		
+		// Visão vertical para baixo
+		i = heroiY;
+		canseefurther = true;
+		while( i < lineLength && canseefurther) {
+			heromap[i][heroiX] = true;
+			if(map[i][heroiX] != null && i != heroiY) {
+				canseefurther = false;
+			}
+			
+			if(wallmap[i][heroiX]  == "baixo" || wallmap[i][heroiX]  == "baixolado") {
+				canseefurther = false;
+			}
+			else {
+				i++;
+			}
+			
+		}
+		
+		//Visão vertical para cima
+		i = heroiY;
+		canseefurther = true;
+		while( i >= 0 && canseefurther) {
+			if(map[i][heroiX] != null && i != heroiY) {
+				canseefurther = false;
+					}
+			
+			if(wallmap[i][heroiX]  == "baixo" || wallmap[i][heroiX]  == "baixolado") {
+				canseefurther = false;
+			}
+			else {
+				heromap[i][heroiX]  = true;
+				i--;
+			}
+			
+		}
+		
+	}
+	
+	public void printHeroMapAAA() {
+		for (int i = 0 ; i < lineLength ; i++) {
+			for (int j = 0 ; j < collumnLength ; j++) {
+				if(heromap[i][j] == true) {
+					System.out.printf("1 ");
+				}
+				else {
+					System.out.printf("0 ");
+				}
+			}
+			System.out.println(" ");
+		}
+		
 	}
 	
 	//pega o entorno do jogador
